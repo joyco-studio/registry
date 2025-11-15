ae37bd09310538790b82ca8d7b991757c1d42f7e
Matias Perez <matiasperezpc@hotmail.com>
2025-11-15 15:50:18 -0300

    Update documentation and remove obsolete files
    - Changed the title and description of the index document to reflect the new JOYCO Shadcn Registry.
    - Removed the test document as it is no longer needed.
    - Updated the index.ts file to exclude the deleted test document from the docs export.

--- .source/index.ts
+++ .source/index.ts
@@ -1,7 +1,6 @@
 // @ts-nocheck -- skip type checking
-import * as d_docs_2 from "../content/docs/test.mdx?collection=docs"
 import * as d_docs_1 from "../content/docs/infinite-list.mdx?collection=docs"
 import * as d_docs_0 from "../content/docs/index.mdx?collection=docs"
 import { _runtime } from "fumadocs-mdx/runtime/next"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","fullPath":"content/docs/index.mdx"}, data: d_docs_0 }, { info: {"path":"infinite-list.mdx","fullPath":"content/docs/infinite-list.mdx"}, data: d_docs_1 }, { info: {"path":"test.mdx","fullPath":"content/docs/test.mdx"}, data: d_docs_2 }], [])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","fullPath":"content/docs/index.mdx"}, data: d_docs_0 }, { info: {"path":"infinite-list.mdx","fullPath":"content/docs/infinite-list.mdx"}, data: d_docs_1 }], [])
