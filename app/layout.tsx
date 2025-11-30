4fc42a1bfab1d120096bbcbff8eea0330adbfa61
Matias Perez <matiasperezpc@hotmail.com>
2025-11-30 02:40:55 -0300

    Refactor layout structure and add new registry components
    - Removed the `DocsLayout` from the main layout and directly rendered children within the `RootProvider`.
    - Introduced a new layout for the registry pages that utilizes `DocsLayout` for rendering.
    - Added a new page component for dynamic routing in the registry, including metadata generation and static parameters.
    - Created a new `Logo` component for consistent branding in the layout shared options.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { baseOptions } from '@/lib/layout.shared';
-import { RootProvider } from 'fumadocs-ui/provider/next';
 import './globals.css';
-import { Inter } from 'next/font/google';
 import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { RootProvider } from 'fumadocs-ui/provider/next';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -34,9 +31,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>
-          <DocsLayout tree={source.pageTree} {...baseOptions()}>
-            {children}
-          </DocsLayout>
+          {children}
         </RootProvider>
       </body>
     </html>
--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,9 +1,17 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { Logo } from "@/components/ui/logo";
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'JOYCO Registry',
+      title: (
+        <div className="flex gap-2 items-center">
+          <div className="bg-background rounded-sm border">
+            <Logo className="w-6 h-6" />
+          </div>{" "}
+          JOYCO Registry
+        </div>
+      ),
     },
   };
 }
