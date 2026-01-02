e4ee1e97bd377503fc111a6beac864effc4d91a7
JOYBOY-0 <falamino00@gmail.com>
2026-01-02 12:09:25 -0300

    add font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import './styles/globals.css'
 import { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Public_Sans, Roboto_Mono } from 'next/font/google'
 import { RootProvider } from 'fumadocs-ui/provider/next'
 import { Analytics } from '@vercel/analytics/next'
+import { cn } from '@/lib/utils'
 
-const inter = Inter({
+const publicSans = Public_Sans({
   subsets: ['latin'],
+  variable: '--font-public-sans',
+})
+
+const robotoMono = Roboto_Mono({
+  subsets: ['latin'],
+  variable: '--font-roboto-mono',
 })
 
 export const metadata: Metadata = {
@@ -33,7 +40,11 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={cn(publicSans.className, robotoMono.className)}
+      suppressHydrationWarning
+    >
       <body className="flex min-h-screen flex-1 flex-col">
         <RootProvider>{children}</RootProvider>
         <Analytics />
