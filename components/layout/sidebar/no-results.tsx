c5980844cfe262171759779c54047fc0db8bcf45
JOYBOY-0 <falamino00@gmail.com>
2026-01-07 10:40:07 -0300

    Update NoResults component layout for improved responsiveness
    ### Changes Made:
    - Added a responsive height class to the NoResults component to enhance layout on larger screens.

--- components/layout/sidebar/no-results.tsx
+++ components/layout/sidebar/no-results.tsx
@@ -9,7 +9,7 @@ type NoResultsProps = {
 export function NoResults({ query }: NoResultsProps) {
   return (
     <>
-      <div className="bg-accent py-4 text-center">
+      <div className="bg-accent lg:h-aside-width py-4 text-center">
         <p className="text-muted-foreground font-mono text-sm font-medium uppercase">
           No results for &ldquo;
           <span className="text-foreground font-bold">{query}</span>&rdquo;
